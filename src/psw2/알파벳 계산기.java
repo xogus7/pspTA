98114d5667e5b4d38227ca2da9574025799c4cbe
xogus7 <70815654+xogus7@users.noreply.github.com>
2021-11-24 12:21:14 +0900

    Update and rename 알파벳 계산기 to 알파벳 계산기.java

--- src/psw2/알파벳 계산기.java
+++ src/psw2/알파벳 계산기.java
@@ -1,8 +1,6 @@
-package psw2;
-
 import java.io.*;
 
-public class p1 {	// 알파벳 계산기
+public class Main {	// 알파벳 계산기
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String s;
