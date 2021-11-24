a7be8405881652e9d51c9f9c965adbd925984a92
xogus7 <70815654+xogus7@users.noreply.github.com>
2021-11-24 12:26:40 +0900

    test

--- src/psw5/UTF-8.java
+++ src/psw5/UTF-8.java
@@ -1,8 +1,6 @@
-package psw5;
-
 import java.io.*;
 
-public class p1 {
+public class Main {
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
