43aab8b0e12f2d72f4d040a56380f422916096de
xogus7 <70815654+xogus7@users.noreply.github.com>
2021-11-24 12:31:08 +0900

    test

--- src/psw6/비토와 친척들 2차원.java
+++ src/psw6/비토와 친척들 2차원.java
@@ -1,31 +1,24 @@
-package psw6;
-
 import java.util.*;
 
 class Point implements Comparable<Point> {
 	int x, y, distance;
-
 	public Point(int x, int y, int vitoX, int vitoY) {
 		this.x = x;
 		this.y = y;
 		this.distance = Math.abs(x - vitoX) + Math.abs(y - vitoY); // 비토와의 거리
 	}
-
 	public int compareTo(Point o) { // 객체 정렬 기준 재정의
 		return this.distance - o.distance; // 이동거리 오름차순
 	}
-
-	public String toString() {
-		return this.x + " " + this.y;
-	}
+	public String toString() { return this.x + " " + this.y; }
 }
 
-public class p2 { // 비토와 친척들 2
+public class Main { // 비토와 친척들 2
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		int n = input.nextInt(); // 집 수
-		int m = input.nextInt(); // 출력 할 수
+		int m = input.nextInt(); // 출력할 수
 		int[] x = new int[n];
 		int[] y = new int[n];
 		int sum = 0;	// 거리 총 합
@@ -35,10 +28,10 @@ public class p2 { // 비토와 친척들 2
 			y[i] = input.nextInt();
 		}
 		int vitoX = quickSort(x.clone()); // 친척집의 x좌표 중앙값
-		int vitoY = quickSort(y.clone()); // y좌표 중앙값
+		int vitoY = quickSort(y.clone());          // y좌표 중앙값
 		for (int i = 0; i < n; i++) {
 			point[i] = new Point(x[i], y[i], vitoX, vitoY); // 친적집 좌표 입력
-			sum += point[i].distance;						// 거리 더하기
+			sum += point[i].distance;			// 거리 더하기
 		}
 		Arrays.sort(point);	// 재정의한 기준대로 정렬
 		System.out.println(sum);
@@ -46,7 +39,7 @@ public class p2 { // 비토와 친척들 2
 			System.out.println(point[i]);
 	}
 
-	/* quickSort 구현 return: 중앙값 */
+	/* quickSort 직접 구현 return: 중앙값 */
 	private static void swap(int[] a, int i, int j) {
 		int temp = a[i];
 		a[i] = a[j];
