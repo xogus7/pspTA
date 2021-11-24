4128bc674087ab24a11758bd2b31b0f7429bc98a
xogus7 <70815654+xogus7@users.noreply.github.com>
2021-11-24 12:17:02 +0900

    Update p2.java

--- src/psw10/p2.java
+++ src/psw10/p2.java
@@ -1,10 +1,32 @@
-package psw10;
+import java.util.*;
 
-public class p2 {
+public class Main {
+	private static boolean[] s;
+	private static int n;
+	private static StringBuilder sb;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner input = new Scanner(System.in);
+		n = input.nextInt();
+		s = new boolean[n * 2 + 1];
+		sb = new StringBuilder();
+		bt(0, 0, 0);
+		System.out.print(sb);
 	}
 
+	public static void bt(int select, int box, int cover) {
+		if (select == n * 2) {
+			for (int i = 0; i < n * 2; i++)
+				if (s[i]) sb.append('b');
+				else sb.append('c');
+			sb.append('\n');
+			return;
+		}
+		s[select] = true;
+		if (box < n)
+			bt(select + 1, box + 1, cover);
+		s[select] = false;
+		if (cover < box)
+			bt(select + 1, box, cover + 1);
+	}
 }
